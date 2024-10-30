0595f34b123efe87909d4457acd9db88e50d7c1e
maksim <m.merkulovdodo@gmail.com>
2024-10-30 16:32:56 +0300

    Создано логирование модулей masks.py, utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,17 @@
 import json
+import logging
 import os
 from typing import Any
+
+# Настройка логера для utils.log
+load_transaction_logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("logs/utils.log", mode="w")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter("%(asctime)s = - %(name)s - %(levelname)s - %(message)s"))
+
+# Добавляем обработчик к логеру
+load_transaction_logger.addHandler(file_handler)
+load_transaction_logger.setLevel(logging.DEBUG)
 
 
 def load_transaction(path_to_json: str) -> Any:
@@ -11,14 +22,21 @@ def load_transaction(path_to_json: str) -> Any:
     :param path_to_json: Путь до JSON-файла с транзакциями.
     :return: Список словарей с данными о транзакциях или пустой список при ошибках.
     """
+    load_transaction_logger.info(f"Загружаем данные из {path_to_json}")
     try:
         if not os.path.exists(path_to_json):
+            load_transaction_logger.error(f"По указанному пути {path_to_json} файла не существует")
             return []
         with open(path_to_json, "r") as file:
             data = json.load(file)
             if isinstance(data, list) and len(data) != 0:
+                load_transaction_logger.info("Данные с файла успешно прочитаны и записаны в data")
                 return data
             else:
+                load_transaction_logger.error("Внутри файла передан не список словарей")
                 return []
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
+        load_transaction_logger.error(
+            f"Декодирование невозможно. Полученные данные не являются JSON-объектом. Ошибка {e}"
+        )
         return []
